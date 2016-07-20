a64e7605819ac1ec50f54b58de8b80a39d23cd45
Caleb Hailey <caleb@heavywater.io>
2016-07-20 13:48:26 -0700

    set version to 0.0.1

--- lib/sensu-plugins-bigpanda/version.rb
+++ lib/sensu-plugins-bigpanda/version.rb
@@ -1,8 +1,8 @@
 module SensuPluginsBigPanda
   module Version
     MAJOR = 0
-    MINOR = 4
-    PATCH = 0
+    MINOR = 0
+    PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
